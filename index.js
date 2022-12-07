75e30c6b85406dc20843c1f4b34ea90222d6c1bc
Helena Tenev <htenev14@gmail.com>
2022-12-07 12:14:04 -0500

    commit tree 96bfc0102306218bdda59139f721dd3352add3e0

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+// const findMatching = filter(names, function (string) {
+//     return string === names;
+// });
+
+// function findMatching(names, string) { 
+//     const filter = filter(check); 
+//     function check(string,names) {
+//         string === names};
+// };
+
+
+function findMatching(names,string) {
+   return names.filter(function (name) {
+    return string.toUpperCase() === name.toUpperCase();
+  })
+};
+
+function fuzzyMatch(names, string) {
+  return names.filter(function (name) {
+    return string.charAt(0) === name.charAt(0);
+  })
+};
+
+
+function matchName(objects, string) {
+  return objects.filter(function (object) {
+    return object['name'] === string;
+  })
+};
